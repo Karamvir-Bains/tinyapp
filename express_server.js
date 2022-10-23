1713a7a35fbebfb262954c41be698047567325f4
Karamvir-Bains <kbains2000@gmail.com>
2022-10-23 15:18:01 -0600

    Add html and rendering for single url

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
