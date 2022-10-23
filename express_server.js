2c0379c7c820cefb54ac3194dab77e5eb33ebbee
Karamvir-Bains <kbains2000@gmail.com>
2022-10-23 17:32:22 -0600

    Add html and rendering for urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -26,6 +26,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
