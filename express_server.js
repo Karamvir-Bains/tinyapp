aeb67beb631e246177c4ecaa99ce2d868a931329
Karamvir-Bains <kbains2000@gmail.com>
2022-11-02 18:31:02 -0600

    Update logout redirect to login page

--- express_server.js
+++ express_server.js
@@ -126,7 +126,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 app.post("/register", (req, res) => {
