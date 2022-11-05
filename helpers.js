79b8b89eb3ffb5a4ca1c2a127398196de92cceed
Karamvir-Bains <kbains2000@gmail.com>
2022-11-05 14:12:26 -0600

    Add mocha/chai test for shortUrlExists & urlsForUser

--- helpers.js
+++ helpers.js
@@ -1,7 +1,5 @@
 const getUserByEmail = function(email, database) {
-  console.log(database);
   for (const user in database) {
-    console.log(database[user]);
     if (database[user].email === email) {
       return database[user];
     }
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,8 +1,8 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, shortUrlExists, urlsForUser } = require('../helpers.js');
 
-const testUsers = {
+const testUserDatabase = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
@@ -15,16 +15,56 @@ const testUsers = {
   }
 };
 
-describe('getUserByEmail', function() {
+const testURLDatabase = {
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID",
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "user2RandomID",
+  },
+};
+
+describe('#getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers);
+    const user = getUserByEmail("user@example.com", testUserDatabase);
     const expectedUserID = "userRandomID";
     const actualID = user.id;
     assert.equal(actualID, expectedUserID);
   });
   it('should return undefined for non-existent user', function() {
-    const user = getUserByEmail("notreal@example.com", testUsers);
+    const user = getUserByEmail("notreal@example.com", testUserDatabase);
     const expectedUserID = undefined;
     assert.equal(user, expectedUserID);
+  });
+});
+
+describe('#shortUrlExists', function() {
+  it('should return true if shortURL found', function() {
+    const found = shortUrlExists("b2xVn2", testURLDatabase);
+    assert.isTrue(found, true);
+  });
+  it('should return false for non-existent shortURL', function() {
+    const found = shortUrlExists("d8wj2x", testURLDatabase);
+    assert.isFalse(found);
+  });
+});
+
+describe('#urlsForUser', function() {
+  it('should return a data object of urls for that user', function() {
+    const userURLs = urlsForUser("user2RandomID", testURLDatabase);
+    const expectedObj = {
+      "9sm5xK": {
+        longURL: "http://www.google.com",
+        userID: "user2RandomID",
+      }
+    };
+    assert.deepEqual(userURLs, expectedObj);
+  });
+  it('should return a empty data object for user without urls', function() {
+    const userURLs = urlsForUser("user3RandomID", testURLDatabase);
+    const expectedObj = {};
+    assert.deepEqual(userURLs, expectedObj);
   });
 });
